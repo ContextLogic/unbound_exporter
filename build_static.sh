9130ff58bb8877218746079ba14ef3602b060c85
Mikhail Rozentsvayg <mrozentsvayg@wish.com>
2020-05-07 18:11:23 -0700

    fix flags for static go build

--- build_static.sh
+++ build_static.sh
@@ -10,6 +10,6 @@ update-ca-certificates
 
 # Build the unbound_exporter.
 cd /unbound_exporter
-go build --ldflags '-extldflags "-static"'
+go build -ldflags "-linkmode external -extldflags -static" -a
 strip unbound_exporter
 EOF
